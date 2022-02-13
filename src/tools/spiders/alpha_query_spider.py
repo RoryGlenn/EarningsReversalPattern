722eeb3dafc139bf81e8527b814dc9d3952bac7b
Rory Glenn <glennrory@gmail.com>
2022-02-13 13:24:07 -0800

    Clean up

--- src/tools/spiders/alpha_query_spider.py
+++ src/tools/spiders/alpha_query_spider.py
@@ -1,9 +1,6 @@
 import datetime
 
 import pandas as pd
-import numpy  as np
-
-from pprint         import PrettyPrinter, pprint
 
 from ..util.enums   import *
 from ..util.globals import G
@@ -13,17 +10,7 @@ class AlphaQuerySpider():
     def __init__(self) -> None:
         self.reported_stocks   = dict()
         self.unreported_stocks = dict()
-        # self.all_stocks        = dict()
         return
-
-    # def __add_to_all_stocks(self, symbol, entry_date) -> None:
-    #     entry_date_str = entry_date[0] + '-' + entry_date[1] + '-' + entry_date[2]
-        
-    #     if symbol in self.all_stocks:
-    #         self.all_stocks[symbol].append(entry_date_str)
-    #     else:
-    #         self.all_stocks[symbol] = [entry_date_str]
-    #     return
 
     def __add_to_reported_stocks(self, symbol, earnings_date) -> None:
         if symbol in self.reported_stocks.keys():
@@ -41,10 +28,9 @@ class AlphaQuerySpider():
             self.unreported_stocks[symbol] = [entry_date_str]
         return
 
-
     def scrape_data(self) -> None:
-        stock_df    = pd.read_excel(STOCK_LIST_TEST, usecols=['Date', 'Symbol', 'Price'])
-        # stock_df    = pd.read_excel(STOCK_LIST, usecols=['Date', 'Symbol', 'Price'])
+        stock_df    = pd.read_excel(STOCK_LIST, usecols=['Date', 'Symbol', 'Price'])
+        # stock_df    = pd.read_excel(STOCK_LIST_TEST, usecols=['Date', 'Symbol', 'Price'])
         symbol_list = stock_df['Symbol'].to_list()
         stock_count = 1
         reported    = False
@@ -64,21 +50,13 @@ class AlphaQuerySpider():
                 end_date    = datetime.datetime(year=int(entry_date[2]), month=int(entry_date[0]), day=int(entry_date[1]))
                 start_date  = end_date - datetime.timedelta(days=7)
 
-                # self.__add_to_all_stocks(symbol, entry_date)
-
                 for _row in earnings_df.iterrows():
                     try:
                         f_quarter_end = [int(i) for i in _row[1]['Fiscal Quarter End'].split('-')]
                         earnings_date = datetime.datetime(year=f_quarter_end[0], month=f_quarter_end[1], day=f_quarter_end[2])
                         
                         # is there an earnings day that falls between start_date and end_date?
-                        if earnings_date < end_date and earnings_date >= start_date:
-                            # did the price go up on or after the earnings date?
-                            # if the stock reported on any of those 5 business days including the previous monday, excluding the entry day
-                            # check for a weekly pattern, not daily pattern
-                            # divide up the two dictionaries, reported and non-reported
-                            # spreadsheet, one with reported and one non-reported
-                            
+                        if earnings_date < end_date and earnings_date >= start_date:                           
                             self.__add_to_reported_stocks(symbol, earnings_date)
                             reported = True
                             break
@@ -93,74 +71,13 @@ class AlphaQuerySpider():
                 G.log.print_and_log(e=e, filename=__file__)
             stock_count += 1
 
-        # reported_stock_str = PrettyPrinter(indent=1).pformat({symbol: earnings_date for (symbol, earnings_date) in self.reported_stocks.items()})
-
         reported_s   = pd.Series(self.reported_stocks)
         unreported_s = pd.Series(self.unreported_stocks)
 
-        reported_s.sort_index()
-        unreported_s.sort_index()
+        G.log.print_and_log(f"Reported stocks\n{reported_s}")
+        G.log.print_and_log(f"Unreported stocks\n{unreported_s}")
 
-        G.log.print_and_log("Reported stocks")
-        G.log.print_and_log(f"{reported_s}")
-
-        G.log.print_and_log(f"Unreported stocks")
-        G.log.print_and_log(f"{unreported_s}")
-
-        writer = pd.ExcelWriter('Trade_Result.xlsx', engine='xlsxwriter')
-
-        reported_s.to_excel(writer, sheet_name="Reported_Stocks")
-        unreported_s.to_excel(writer, sheet_name="Unreported_Stocks")
-
-        writer.save()
+        with pd.ExcelWriter('Trade_Result.xlsx', engine='xlsxwriter') as writer:
+            reported_s.to_excel(writer, sheet_name="Reported_Stocks")
+            unreported_s.to_excel(writer, sheet_name="Unreported_Stocks")
         return
-
-
-
-"""
-    {'ALLE': ['12-31-2020'],
-    'APTV': ['09-30-2020'],
-    'ATVI': ['09-30-2019'],
-    'BDX': ['06-30-2018', '09-30-2018', '03-31-2019'],
-    'BIO': ['03-31-2019'],
-    'CLX': ['06-30-2020'],
-    'COST': ['11-30-2019'],
-    'CTSH': ['03-31-2018'],
-    'DD': ['12-31-2017'],
-    'DIS': ['06-30-2019'],
-    'DLR': ['09-30-2017'],
-    'DXCM': ['03-31-2017'],
-    'EL': ['09-30-2017', '09-30-2021'],
-    'EMN': ['03-31-2017'],
-    'EPAM': ['06-30-2018'],
-    'EQR': ['12-31-2021'],
-    'FB': ['09-30-2017'],
-    'FLT': ['03-31-2017'],
-    'FRT': ['12-31-2021'],
-    'HES': ['12-31-2017'],
-    'IT': ['06-30-2021'],
-    'J': ['09-30-2019'],
-    'JKHY': ['06-30-2017', '09-30-2021'],
-    'LIN': ['12-31-2021'],
-    'LOW': ['04-30-2019'],
-    'MLM': ['12-31-2020'],
-    'MSCI': ['06-30-2020'],
-    'NDAQ': ['09-30-2021'],
-    'NUE': ['12-31-2021', '12-31-2021'],
-    'PEP': ['09-30-2019'],
-    'PKI': ['09-30-2020'],
-    'REG': ['12-31-2021'],
-    'ROK': ['06-30-2017'],
-    'ROP': ['06-30-2021'],
-    'SEE': ['06-30-2020'],
-    'SNPS': ['07-31-2019'],
-    'SPGI': ['03-31-2018'],
-    'TDG': ['06-30-2018'],
-    'TMO': ['09-30-2019'],
-    'TSN': ['12-31-2021'],
-    'TT': ['12-31-2020'],
-    'VRSK': ['09-30-2019'],
-    'WST': ['09-30-2019'],
-    'XOM': ['06-30-2021']}
-
-"""        
